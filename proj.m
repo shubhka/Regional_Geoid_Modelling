9b121cd0e3ac89d91053cb625c5c2acb868210cc
Shubhi Kant <shubhikant.sk@gmail.com>
2022-10-31 15:16:42 +0530

    Study area changed to AN01

--- proj.m
+++ proj.m
@@ -1,4 +1,4 @@
-fid = fopen ("NGS_GRAVD_Block_PN02_Gravity_Data_BETA1.txt", "r");
+fid = fopen ("NGS_GRAVD_Block_AN01_Gravity_Data_BETA1.txt", "r");
 % Reading header files into a vector of strings
 %header = textscan (fid,'%s', 21, 'Delimiter','\n');
 %header = [header{:}];
@@ -6,7 +6,7 @@ fid = fopen ("NGS_GRAVD_Block_PN02_Gravity_Data_BETA1.txt", "r");
 %disp(header);
 
 % Reading data into a matrix
-data = textscan (fid,'PN02%d%d%f%f%f%f\n','Delimiter',' ','MultipleDelimsAsOne',1, 'CommentStyle', '/');
+data = textscan (fid,'AN01%d%d%f%f%f%f\n','Delimiter',' ','MultipleDelimsAsOne',1, 'CommentStyle', '/');
 lines = [data(1){:}];
 datas = cell2mat(cellfun(@double, data(2:end), 'uni', false));
 lat = [data(3){:}];
@@ -18,7 +18,7 @@ normal_grav = compute_normal_grav(lat, 'WGS84');
 FAA = compute_free_air_anomaly(obs_grav, lat, elevation, 'WGS84');
 
 %% 4)
-fid = fopen ("GGM05S.gdf", "r");
+fid = fopen ("GGM05C_AN01.gdf", "r");
 ggm = textscan (fid,'%f%f%f\n','Delimiter',' ','MultipleDelimsAsOne',1, 'HeaderLines',36);
 lon_ggm = [ggm(1){:}]-360;
 lat_ggm = [ggm(2){:}];
